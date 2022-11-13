68367534c4504d1b1e51a9cd46d28da94a4f5828
Olga Zarosylova <zoa1906@gmail.com>
2022-11-13 19:49:19 +0200

    //lf

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,11 +3,15 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public static void main(String[] args) {
         NumbersPrinter numbersPrinter = new NumbersPrinter();
-        numbersPrinter.printIsPositive(15);
+        numbersPrinter.printIsPositive(18);
     }
-    public void printIsPositive(int number) {
-        boolean numberOne = (number % 2 == 0) ? true : false;
-        System.out.println(numberOne);
 
+    public void printIsPositive(int number) {
+        if (number % 2 == 0) {
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
     }
 }
